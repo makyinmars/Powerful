399e531445a337ed15bc97f07be4700c327683bd
Maky <franklinjaradev@gmail.com>
2021-12-15 19:10:17 -0500

    chore: progress history style and progressApi added

--- client/pages/progress/user/[userId].tsx
+++ client/pages/progress/user/[userId].tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useGetAllProgressByUserQuery } from "../../../app/services/progressApi";
+import SuccessQueryHandling from "../../../components/successQuery";
+import Spinner from "../../../components/spinner";
+import ErrorQueryHandling from "../../../components/errorQuery";
 
 const AllProgressByUserId = () => {
   const router = useRouter();
@@ -10,25 +14,46 @@ const AllProgressByUserId = () => {
   const { data, isError, isLoading, isSuccess, error } =
     useGetAllProgressByUserQuery(userId as string);
 
-  console.log(data);
   return (
     <>
       <h1 className="heading-brand">Your progress history</h1>
 
       <div className="flex justify-center">
-        <button className="button-brand w-auto">Create a new progress</button>
+        <button className="button-brand w-auto">
+          <Link href="/progress">Create a new progress</Link>
+        </button>
       </div>
 
-      <div className="container mx-auto">
-        <div className="grid grid-cols-1 sm:grid-cols-2">
+      {/* Status */}
+      {isLoading && <Spinner />}
+      {isError ? <ErrorQueryHandling error={error} /> : null}
+      {isSuccess && (
+        <SuccessQueryHandling text="Progress history loaded successfully" />
+      )}
+
+      <div className="container-brand-card">
+        <div className="card-brand">
           {data?.map((progress, index) => (
-            <div key={index} className="">
-              <div className="">
-                <img
-                  className="w-96 h-60 rounded-3xl"
-                  src={progress.picture}
-                  alt={progress.description}
-                />
+            <div key={index} className="p-1 bg-brand-600 rounded">
+              <img
+                className="w-80 h-80 rounded"
+                src={progress.picture}
+                alt={progress.description}
+              />
+              <div className="card-brand-attributes">
+                <div className="self-center">
+                  <button className="button-brand">
+                    <Link href={`/progress/${progress.id}`}>Edit progress</Link>
+                  </button>
+                </div>
+                <div className="justify-self-center">
+                  <h2 className="title-brand">Weight</h2>
+                  <p>{progress.weight} lbs</p>
+                </div>
+              </div>
+              <div className="card-brand-description">
+                <h2 className="font-bold text-lg">Description</h2>
+                <p>{progress.description}</p>
               </div>
             </div>
           ))}
