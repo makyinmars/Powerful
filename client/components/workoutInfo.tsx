910a1b2f8526a7c1f4e188850ad3e36ce5f48343
Maky <franklinjaradev@gmail.com>
2021-12-17 12:17:52 -0500

    chore: working on workout id

--- client/components/workoutInfo.tsx
+++ client/components/workoutInfo.tsx
@@ -12,19 +12,37 @@ const WorkoutInfo = ({ data, isLoading, isError, error }: WorkoutInfoProps) => {
   // Get the workout id from data
   const { id, userId } = data;
 
+  console.log(data);
+
   return (
     <>
       <h1 className="heading-brand">Workout Information</h1>
 
-      <h2 className="title-brand text-center">{data.name}</h2>
+      <h2 className="text-center title-brand">{data.name}</h2>
       {/* Add Exercise */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 bg-red-400">
+      <div className="grid grid-cols-1 gap-2 bg-red-400 md:grid-cols-2 xl:grid-cols-3">
         {data.exercises?.map((exercise, index) => (
           <ul
             key={index}
-            className="flex flex-col sm:flex-row justify-center items-center shadow-lg"
+            className="flex flex-col justify-center shadow-lg md:flex-row"
           >
-            <li>{exercise.name}</li>
+            <li className="self-center">{exercise.name}</li>
+            <li className="justify-self-stretch">
+              <h3>Weight</h3>
+              <h3>Reps</h3>
+            </li>
+            {exercise.sets?.map((set, index) => (
+              <ul key={index} className="grid grid-cols-3 grid-rows-1">
+                <li>
+                  <h3>Reps</h3>
+                  <p>{set.reps}</p>
+                </li>
+                <li>
+                  <h3>Weight</h3>
+                  <p>{set.weight}</p>
+                </li>
+              </ul>
+            ))}
           </ul>
         ))}
       </div>
